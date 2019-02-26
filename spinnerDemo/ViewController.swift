033e0d381120a42e32c271d748b582e1a8c184ba
Phani_Kiran <pret321@gmail.com>
2019-02-25 18:15:39 -0600

    commit

--- spinnerDemo/ViewController.swift
+++ spinnerDemo/ViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func startButton(_ sender: Any) {
+        activityView.startAnimating()
+        activityView.color = .green
+    }
+    @IBAction func stopButton(_ sender: Any) {
+        activityView.stopAnimating()
+        activityView.color = .red
+        
+    }
+    
+    
+    
+    @IBOutlet weak var activityView: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
